25eb4a3f996b4af18ac5e4b882f138d7a7d70983
Neil Christian Riego <neilchristianriego3@gmail.com>
2023-05-18 05:08:40 +0800

    feat: Add calculator.py with 4 functions

---
+++ src/calculator.py
@@ -0,0 +1,14 @@
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    return x - y
+
+
+def multiply(x: int, y: int) -> int:
+    return x * y
+
+
+def divide(x: int, y: int) -> float:
+    return x / y
